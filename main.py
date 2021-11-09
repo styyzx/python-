ec67d5be602849502114f488a81199d0a145b964
王凤丽 <1326461897@qq.com>
2021-11-10 07:09:23 +0800

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+import pandas as pd
+import tushare as ts
+from sqlalchemy import create_engine
+
+from mysql_tables_structure import Base
+import mysql_functions as mf
+
+# 创建数据库引擎
+engine = create_engine(mysql://root:****@127.0.0.1/databasename?charset=utf8mb4)
+conn = engine.connect()
+
+# 创建mysql所有表结构
+Base.metadata.create_all(engine)
+
+# 连接 tushare
+ts.set_token(token****)
+pro = ts.pro_api()
+
+# 股票列表
+mf.update_stock_basic(engine, pro, 3, 2)
+
+
+## https://www.52pojie.cn/thread-1073645-1-1.html
